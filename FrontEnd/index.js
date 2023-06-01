c6de63570421e7956a79fc34babd57b1b4b4ebae
j.crudenaire@laposte.net <j.crudenaire@laposte.net>
2023-06-01 16:04:26 +0200

    finition deuixiéme modal , affichage ok

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,6 +3,7 @@ const logoutButton = document.getElementById('logout');
 const token = localStorage.getItem('token');
 const linkModals = document.getElementsByClassName("link__modal")
 const modalLink = document.querySelector(".link__modal__porfolio");
+
 //----------- Récupération des API -------------------------
 function init() {
     fetch("http://localhost:5678/api/works")
@@ -126,6 +127,7 @@ function deleteImage(imageId) {
         .catch(error => {
             console.log("Error delete picture:", error);
         })
+    init();
 }
 //espace administrateur
 
@@ -161,50 +163,85 @@ modalLink.addEventListener('click', () => {
 });
 
 // click ajout photo ---Création de la deuxiéme modal 
+
 const fileInput = document.getElementById('add__picture');
-fileInput.addEventListener('click', () => {
+fileInput.addEventListener('click', (event) => {
     event.preventDefault();
     const modalWrapper = document.querySelector('.modal__wrapper')
     fetch("http://localhost:5678/api/categories")
         .then((response) => response.json())
         .then(categories => {
             modalWrapper.innerHTML = `
+    
         <i class="fa-solid fa-arrow-left"></i>
         <i class="fa-solid fa-xmark close__icon"></i>
+        <div class=add__global>
         <h3 id="title_modal">Ajout photo </h3>
         <div class="add__picture">
         <i class="fa-regular fa-image"></i>
         <form action="#" method="post">
         <input type="file" id="modal__add__picture" value="Ajouter une photo" /> <br>
-        <label for="fileInput" id="modal__add__picture">+ Ajouter une photo</label>
+        <label for="modal__add__picture" id="modal__add__picture">+ Ajouter une photo</label>
         </form>
         <p> jpg, png : 4mo max</p>
         </div>
+        <div id="modal__title__categorie">
         <form action="#" method="post">
         <label for="name">Titre</label>
         <input type="text" name="name" id="name" />
         <label for="categories"> Catégorie </label>
-        <select id="categories"> name="categories">
-        ${categories.map(category => `<option value="${category.name}">${category.name}</option>`)}
+        <select id="categories" name="categories">
+        <option value= " " > </option>
+        ${categories.map(category => `<option value="${category.id}">${category.name}</option>`)}
         </select>
         </form>
+        </div>
+        <form action="#" method="post">
+        <input class="submit" type="submit" id="valid" value="Valider" />
+      </form>
+        </div>
         `;
+            addPictureInput();
         })
 });
-/*
-// Récupération de l'image administrateur --------------------------------------
-const modalGallery = document.querySelector('.gallery__modal');
-console.log(fileInput);
 
-fileInput.addEventListener('change', (e) => {
-    const selectFile = e.target.files[0];
-    console.log(selectFile);
-    const newFile = new FileReader();
-    newFile.addEventListener('load', (e) => {
-        const addImage = document.createElement('img')
-        addImage.src = e.target.result;
-        addImage.classList.add('.gallery__modal')
-        modalGallery.appendChild(addImage)
-    })
-    newFile.readAsDataURL(selectFile);
-})*/
+function addPictureInput() {
+    const addInput = document.querySelector("#modal__add__picture");
+    const divAddPicture = document.querySelector(".add__picture")
+    const fileInput = divAddPicture.querySelector('input[type=file]');
+    const labelInput = divAddPicture.querySelector('label');
+    const paragraphInput = divAddPicture.querySelector('p');
+    const iconeInput = divAddPicture.querySelector('i');
+    const titleInput = document.getElementById('name');
+    const categoriesInput = document.getElementById('categories');
+    const submitButton = document.getElementById('valid');
+    console.log(titleInput);
+    console.log(addInput);
+    console.log(categoriesInput);
+    addInput.addEventListener('change', (e) => {
+        const selectFile = e.target.files[0];
+        console.log(selectFile);
+        const newFile = new FileReader();
+        newFile.addEventListener('load', (e) => {
+            const addImage = document.createElement('img')
+            addImage.src = e.target.result;
+            addImage.classList.add('add__img__display');
+            divAddPicture.appendChild(addImage);
+            fileInput.style.display = 'none';
+            labelInput.style.display = 'none';
+            paragraphInput.style.display = 'none';
+            iconeInput.style.display = 'none';
+            divAddPicture.style.flexDirection = 'revert';
+            const isImagePresent = divAddPicture.querySelector('img');
+            const isTitlePresent = titleInput.value.trim() !== ' ';
+            const isCategorySelected = categoriesInput.value !== ' ';
+
+            if (isImagePresent && isTitlePresent && isCategorySelected) {
+                submitButton.style.background = '#1D6154'
+            }
+
+        });
+        newFile.readAsDataURL(selectFile);
+
+    });
+};
